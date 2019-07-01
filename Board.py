1cd250411cdd26520a6b3fe3f7098f084324b720
Rajesh <rajesh@esavertec.com>
2019-07-01 22:32:35 +0530

    added valid position track in Board.py

--- Board.py
+++ Board.py
@@ -5,6 +5,8 @@ class Board:
     def __init__(self):
         self.board_length = 7
         self.board = [[Box() for _ in range(self.board_length)] for _ in range(self.board_length)]
+        self.valid_positions = []
+        self.loadValidPositions()
         self.loadDefaultBoard()
 
     def displayBoard(self):
@@ -13,57 +15,62 @@ class Board:
                 box = self.board[row][col]
                 print_symbol = (box.symbol if box.symbol else "O") if box.is_allowed else "-"
                 print(print_symbol,end=" ")
-            print("\n")
+            print("")
+            
+    '''
+    loads the used box position in valid_positions
+    '''            
+    def loadValidPositions(self):
+        self.valid_positions = []
 
+        # first row
+        self.valid_positions.append(Postion(0, 0))
+        self.valid_positions.append(Postion(0, 3))
+        self.valid_positions.append(Postion(0, 6))
+
+        # second row
+        self.valid_positions.append(Postion(1, 1))
+        self.valid_positions.append(Postion(1, 3))
+        self.valid_positions.append(Postion(1, 5))
+
+        # third row
+        self.valid_positions.append(Postion(2, 2))
+        self.valid_positions.append(Postion(2, 3))
+        self.valid_positions.append(Postion(2, 4))
+
+        # fourth row
+        self.valid_positions.append(Postion(3, 0))
+        self.valid_positions.append(Postion(3, 1))
+        self.valid_positions.append(Postion(3, 2))
+        self.valid_positions.append(Postion(3, 4))
+        self.valid_positions.append(Postion(3, 5))
+        self.valid_positions.append(Postion(3, 5))
+        self.valid_positions.append(Postion(3, 6))
+
+        # fifth row
+        self.valid_positions.append(Postion(4, 2))
+        self.valid_positions.append(Postion(4, 3))
+        self.valid_positions.append(Postion(4, 4))
+
+        # sixth row
+        self.valid_positions.append(Postion(5, 1))
+        self.valid_positions.append(Postion(5, 3))
+        self.valid_positions.append(Postion(5, 5))
+
+        # seventh row
+        self.valid_positions.append(Postion(6, 0))
+        self.valid_positions.append(Postion(6, 3))
+        self.valid_positions.append(Postion(6, 6))
+        
     '''
     making the some of the boxes as usable boxes
     '''
     def loadDefaultBoard(self):
-        boxes_to_use = []
 
-        # first row
-        boxes_to_use.append(Postion(0, 0))
-        boxes_to_use.append(Postion(0, 3))
-        boxes_to_use.append(Postion(0, 6))
-
-        # second row
-        boxes_to_use.append(Postion(1, 1))
-        boxes_to_use.append(Postion(1, 3))
-        boxes_to_use.append(Postion(1, 5))
-
-        # third row
-        boxes_to_use.append(Postion(2, 2))
-        boxes_to_use.append(Postion(2, 3))
-        boxes_to_use.append(Postion(2, 4))
-
-        # fourth row
-        boxes_to_use.append(Postion(3, 0))
-        boxes_to_use.append(Postion(3, 1))
-        boxes_to_use.append(Postion(3, 2))
-        boxes_to_use.append(Postion(3, 4))
-        boxes_to_use.append(Postion(3, 5))
-        boxes_to_use.append(Postion(3, 5))
-        boxes_to_use.append(Postion(3, 6))
-
-        # fifth row
-        boxes_to_use.append(Postion(4, 2))
-        boxes_to_use.append(Postion(4, 3))
-        boxes_to_use.append(Postion(4, 4))
-
-        # sixth row
-        boxes_to_use.append(Postion(5, 1))
-        boxes_to_use.append(Postion(5, 3))
-        boxes_to_use.append(Postion(5, 5))
-
-        # seventh row
-        boxes_to_use.append(Postion(6, 0))
-        boxes_to_use.append(Postion(6, 3))
-        boxes_to_use.append(Postion(6, 6))
-
-        for poistion in boxes_to_use:
+        for poistion in self.valid_positions:
             box = self.board[poistion.row][poistion.column]
             # making this box can be used one
             box.updateisAllowed()
 
-#b1 = Board()
-#b1.displayBoard()
+b1 = Board()
+b1.displayBoard()
---
+++ Player.py
@@ -0,0 +1,4 @@
+class Player:
+    def __init__(self):
+        self.no_of_coins = 9
+        self.times_can_place_coin = self.no_of_coins
